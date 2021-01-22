a12e8e902c51bae2d1229a2cc9dd712e5958b7da
murphy <987127314@qq.com>
2021-01-22 09:34:17 +0800

    add component card

--- src/components/Button1.demo.vue
+++ src/components/Button1.demo.vue
@@ -6,13 +6,17 @@
     <Button theme="button">普通按钮</Button>
     <Button theme="link">链接按钮</Button>
     <Button theme="text">文本按钮</Button>
+    <Card title="按钮类型" content="按钮有三种类型：普通按钮、链接按钮和文本按钮。"/>
 </template>
 
 <script lang="ts">
     import Button from "../lib/Button.vue";
+    import Card from "./Card.vue";
+    import Card from './Card.vue';
     export default {
         name: "Button1.demo.vue",
         components:{
+            Card,
             Button
         }
     }
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cardWrapper">
+    <h1>{{title}}</h1>
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Card",
+    props: {
+      title: {
+        type: String
+      },
+      content: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .cardWrapper {
+    margin-top: 20px;
+
+    &::before {
+      content: '';
+      display: block;
+      height: 1px;
+      width: 100%;
+      background: #d9d9d9;
+      position: relative;
+      top: 11px;
+      left: 0;
+    }
+
+    > h1 {
+      display: inline-block;
+      margin-left: 20px;
+      background: #ffffff;
+      font-size: 16px;
+      padding: 0px 18px 0 18px;
+      position: relative;
+    }
+
+    > p {
+      font-size: 14px;
+      padding-top: 5px;
+    }
+  }
+</style>
